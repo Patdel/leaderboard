853cdd07289d7aeea0a2a39144240f7e6e195353
Patricio Calderon <ourobor@gmail.com>
2016-01-07 20:53:11 -0500

    creates first helpers functions

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,20 @@
 
 console.log("Hello world!");
 
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function() {
+      return "Some other text";
+    },
+    'otherHelperFunction': function() {
+       return "Some other function";
+    }
+  });
+
+}
+
+if(Meteor.isServer) {
+
+}
+
 PlayersList = new Mongo.Collection('players');
