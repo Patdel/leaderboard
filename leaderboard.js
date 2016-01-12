506d0e15631e1bf9c9103ea306e41ae7d3696485
Patricio Calderon <ourobor@gmail.com>
2016-01-11 20:51:06 -0500

    adds update and sorting in the database

--- leaderboard.js
+++ leaderboard.js
@@ -4,10 +4,38 @@ console.log("Hello world!");
 if (Meteor.isClient) {
   Template.leaderboard.helpers({
     'player': function() {
-      return "Some other text";
+      return PlayersList.find({}, {sort: {score: -1, name: 1} });
     },
     'otherHelperFunction': function() {
-       return "Some other function";
+
+    },
+    'numberOfPlayers': function() {
+       return PlayersList.find().count();
+    },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId === selectedPlayer) {
+        return "selected";
+      }
+
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function() {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer =  Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    },
+    'click .increment': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .decrement': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
     }
   });
 
