0003b7341999ed6251ffbfa5f99051e9b6edba1d
Patricio Calderon <ourobor@gmail.com>
2016-01-11 21:17:28 -0500

    adds a way to retrieve individual documents from the db

--- leaderboard.js
+++ leaderboard.js
@@ -18,7 +18,10 @@ if (Meteor.isClient) {
       if (playerId === selectedPlayer) {
         return "selected";
       }
-
+    },
+    'showSelectedPlayer': function() {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
 
