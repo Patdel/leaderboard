f220e28ee04fe538e2369221068b6ccb1c884050
Patricio Calderon <ourobor@gmail.com>
2016-01-23 21:53:06 -0500

    takes code out of isClient to put into client file

--- leaderboard.js
+++ leaderboard.js
@@ -3,60 +3,7 @@ console.log("Hello world!");
 
 if (Meteor.isClient) {
 
-  Meteor.subscribe('thePlayers');
 
-  Template.leaderboard.helpers({
-    'player': function() {
-      var currentUserId = Meteor.userId();
-      return PlayersList.find({createdBy: currentUserId}, {sort: {score: -1, name: 1} });
-    },
-    'otherHelperFunction': function() {
-
-    },
-    'numberOfPlayers': function() {
-       return PlayersList.find().count();
-    },
-    'selectedClass': function() {
-      var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer');
-      if (playerId === selectedPlayer) {
-        return "selected";
-      }
-    },
-    'showSelectedPlayer': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      return PlayersList.findOne(selectedPlayer);
-    }
-  });
-
-  Template.leaderboard.events({
-    'click .player': function() {
-      var playerId = this._id;
-      Session.set('selectedPlayer', playerId);
-      var selectedPlayer =  Session.get('selectedPlayer');
-      console.log(selectedPlayer);
-    },
-    'click .increment': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
-    },
-    'click .decrement': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
-    },
-    'click .remove': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      Meteor.call('removePlayerData', selectedPlayer);
-    }
-  });
-
-  Template.addPlayerForm.events({
-    'submit form': function(event) {
-      event.preventDefault();
-      var playerNameVar = event.target.playerName.value;
-      Meteor.call('insertPlayerData', playerNameVar);
-    }
-  });
 
 }
 
